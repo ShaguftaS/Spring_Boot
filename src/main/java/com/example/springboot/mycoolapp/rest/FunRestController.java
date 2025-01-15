df08b7b39e0922acf89d09f8e945a2a85fdf024a
ShaguftaS <shaguftha.96@gmail.com>
2025-01-15 22:02:09 +0530

    Spring boot

--- src/main/java/com/example/springboot/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/example/springboot/mycoolapp/rest/FunRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
     // expose "/" that return "Hello World"
-    @GetMapping("/")
+    @GetMapping(" /")
     public String sayHello() {
         return " Hello  World!";
     }
