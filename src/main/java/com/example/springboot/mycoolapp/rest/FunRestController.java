205e53f419dc136555834c337683797d998ba5dd
ShaguftaS <shaguftha.96@gmail.com>
2025-01-20 18:37:25 +0530

    Spring boot

--- src/main/java/com/example/springboot/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/example/springboot/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,19 @@
 package com.example.springboot.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //inject properties for: coach.name and team.name
+    @Value("${coach.name")
+    private String coachName;
+
+    @Value("${team.name")
+    private String teamName;
+
     // expose "/" that return "Hello World"
     @GetMapping(" /")
     public String sayHello() {
