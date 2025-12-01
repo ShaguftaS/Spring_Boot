3d92d52ce76bf3460562b914fd9748bd705a544d
ShaguftaS <shaguftha.96@gmail.com>
2025-12-01 22:29:25 +0530

    Spring boot

--- src/main/java/Earth.java
+++ src/main/java/Earth.java
@@ -2,8 +2,14 @@ public class Earth {
 
     public static void main(String []args){
 
-        Human tom;
-        tom = new Human();
+        Human shagufta;
+        shagufta = new Human();
+        shagufta.age = 5;
+        shagufta.height = 88;
+        shagufta.name = "Shagufta";
+        shagufta.eyeColor = "Blue";
+
+        shagufta.speak();
 
 
 
