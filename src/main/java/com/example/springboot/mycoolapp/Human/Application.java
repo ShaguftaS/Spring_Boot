b463ee9d719f210958c4dfc14e50d0d1866d1fd8
ShaguftaS <shaguftha.96@gmail.com>
2025-12-02 22:21:07 +0530

    Spring boot

---
+++ src/main/java/com/example/springboot/mycoolapp/Human/Application.java
@@ -0,0 +1,23 @@
+package com.example.springboot.mycoolapp.Human;
+
+import java.util.ArrayList;
+
+public class Application {
+
+    public static void main(String[] args){
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("hello");
+        words.add("there");
+
+        String i1 = words.get(0);
+        System.out.println(i1);
+
+
+
+    }
+
+
+
+
+}
