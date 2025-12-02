f82945435bd5a99968ec1e3e33d69b50c5f4944a
ShaguftaS <shaguftha.96@gmail.com>
2025-12-02 22:44:27 +0530

    Spring boot

--- src/main/java/com/example/springboot/mycoolapp/Human/Application2.java
+++ src/main/java/com/example/springboot/mycoolapp/Human/Application2.java
@@ -11,13 +11,27 @@ public class Application2 {
         ani.add("Cat");
         ani.add("Dog");
 
-        for(int i=0; i < ani.size(); i++){
-            System.out.println(ani.get(i));
-        }
+//        for(int i=0; i < ani.size(); i++){
+//            System.out.println(ani.get(i));
+//        }
+//
+//        for(String an: ani){
+//
+//            System.out.println(an);
+//        }
 
-        for(String an: ani){
+        ArrayList<Vehicle> veh = new ArrayList<Vehicle>();
+        Vehicle veh2 = new Vehicle("Toyota", "Camery", 12500, false);
+        veh.add(new Vehicle("Honda", "accord", 12000, false ));
+        veh.add(veh2);
+        veh.add(new Vehicle("Jeep", "Wrangler", 14555, true));
 
-            System.out.println(an);
+        for(Vehicle c : veh){
+
+            System.out.println(c);
+            System.out.println(c.getMake());
+            System.out.println(c.fourWDrive);
+            System.out.println(c.model);
         }
 
     }
