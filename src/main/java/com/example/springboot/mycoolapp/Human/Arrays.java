08626232cbd795608e93bda76a38527db370b07e
ShaguftaS <shaguftha.96@gmail.com>
2025-12-02 21:47:55 +0530

    Spring boot

---
+++ src/main/java/com/example/springboot/mycoolapp/Human/Arrays.java
@@ -0,0 +1,18 @@
+package com.example.springboot.mycoolapp.Human;
+
+public class Arrays {
+
+    public static void main(String []args){
+
+        int []values = new int[100];
+        values[0] = 1000;
+        values[99] = 9876;
+
+
+        System.out.println(values[1]);
+
+
+
+    }
+
+}
