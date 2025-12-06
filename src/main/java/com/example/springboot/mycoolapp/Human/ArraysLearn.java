82a9dc5d2d9c5828ac3044835b82350220609d56
ShaguftaS <shaguftha.96@gmail.com>
2025-12-06 12:44:48 +0530

    Spring boot

--- src/main/java/com/example/springboot/mycoolapp/Human/ArraysLearn.java
+++ src/main/java/com/example/springboot/mycoolapp/Human/ArraysLearn.java
@@ -1,10 +1,24 @@
 package com.example.springboot.mycoolapp.Human;
 
+import java.util.ArrayList;
+
 public class ArraysLearn {
 
     public static void main(String []args){
 
         int value[] = new int[100];
 
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        list1.add(12);
+        list1.add(323);
+        list1.add(878);
+        list1.add(454);
+
+        ArrayList<Integer> newlist = new ArrayList<Integer>();
+        newlist.add(10);
+
+        list1.addAll(newlist);
+
+        System.out.println(list1);
     }
 }
