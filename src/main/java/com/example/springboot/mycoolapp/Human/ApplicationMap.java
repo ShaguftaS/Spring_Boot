4ffcc34f63cfa6319204ad72c602784bfd94f04a
ShaguftaS <shaguftha.96@gmail.com>
2025-12-07 12:52:16 +0530

    Spring boot

---
+++ src/main/java/com/example/springboot/mycoolapp/Human/ApplicationMap.java
@@ -0,0 +1,22 @@
+package com.example.springboot.mycoolapp.Human;
+
+import java.util.HashMap;
+
+public class ApplicationMap {
+
+    public static void main(String[] args){
+
+        HashMap<String, String> dict = new HashMap<String, String>();
+        dict.put("Brave","ready to face to face ");
+        dict.put("Brilliant","clever or talented ");
+        dict.put("Joy","a feeling of great pleasure");
+        dict.put("Confidence","state of feeling certain about the truth");
+
+        for(String word: dict.keySet()){
+            System.out.println(word);
+
+        }
+
+
+    }
+}
